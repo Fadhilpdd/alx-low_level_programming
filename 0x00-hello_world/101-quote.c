cdbc8cf5804658dfa8b5cbc1df0bcba6d0f3df6b
Fadhilpdd <fadhilabdullah15@gmail.com>
2023-02-20 03:01:14 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - program entry point
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is usefl\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
