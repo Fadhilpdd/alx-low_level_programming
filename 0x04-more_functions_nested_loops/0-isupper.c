92421c12e2c94d127847c69db4a92926b1ea4c8d
Fadhilpdd <fadhilabdullah15@gmail.com>
2023-02-23 10:02:19 -0800

    Task 2

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,17 @@
 
 /**
  * _isupper - uppercase letters
- * @x: char to check
- *
+ * @c: char to check
  * Return 1 for upper letter or 0 for any else
  */
-
-int _isupper(int x)
-
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
 	return (1);
 	}
+	else
+	{
 	return (0);
+	}
 }
